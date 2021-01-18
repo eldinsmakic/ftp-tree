5e6652e146e49d000059b9482f1a5a54616fe945
eldin <eldinsmakic@hotmail.fr>
2021-01-18 19:46:32 +0100

    Move List Command to File

---
+++ ftp-tree/Commands/List.swift
@@ -0,0 +1,30 @@
+//
+//  List.swift
+//  ftp-tree
+//
+//  Created by eldin smakic on 18/01/2021.
+//
+
+import Foundation
+import Network
+
+class List: Command {
+    override init(nwConnection: NWConnection) {
+        super.init(nwConnection: nwConnection)
+        self.validResponse = 150
+        self.commandName = "LIST"
+    }
+
+    override func launch() throws -> String? {
+        let request = "\(self.commandName)"
+        var data = request.data(using: .utf8)!
+        data.append(contentsOf: [13,10])
+        self.send(data: data)
+        self.setupReceive()
+        return try super.launch()
+    }
+
+    override func returnResultCommand(message: String) {
+        result = message
+    }
+}
