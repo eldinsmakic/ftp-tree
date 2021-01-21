6ef1176d9afa0b30eeca48f4662e879631b844f4
Smakic Eldin <eldinsmakic@hotmail.fr>
2021-01-21 18:54:01 +0100

    [Feat]: Add CD command

---
+++ ftp-tree/Commands/CD.swift
@@ -0,0 +1,35 @@
+//
+//  CD.swift
+//  ftp-tree
+//
+//  Created by eldin smakic on 20/01/2021.
+//
+
+import Foundation
+import Network
+
+class CD: Command {
+    var directory: String
+
+     init(nwConnection: NWConnection, directory: String) {
+        self.directory = directory
+
+        super.init(nwConnection: nwConnection)
+        self.validResponse = 250
+        self.commandName = "CWD"
+    }
+
+    override func launch() throws -> String? {
+        let request = "\(self.commandName) \(self.directory)"
+        var data = request.data(using: .utf8)!
+        data.append(contentsOf: [13,10])
+        self.send(data: data)
+        self.setupReceive()
+        return try super.launch()
+    }
+
+    override func returnResultCommand(message: String) {
+        result = message
+    }
+}
+
